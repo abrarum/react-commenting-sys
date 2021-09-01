7f1bc51c443475660a71fc129f925ce91d1f9926
abrarum <abraracs@gmail.com>
2021-09-01 16:09:25 +0200

    added the comment component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import GenComment from "./components/GenComment";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Discuss!</h1>
+      <GenComment />
+    </div>
+  );
+}
---
+++ src/components/GenComment.tsx
@@ -0,0 +1,125 @@
+import { useEffect, useState } from "react";
+
+export default function GenComment(): JSX.Element {
+  type T_Comment = {
+    id: string;
+    content: string;
+    replies: T_Comment[];
+  };
+
+  let initialCommentTree: T_Comment[] = [
+    {
+      id: "0",
+      content: "Please share your opinion on my new car?",
+      replies: [
+        {
+          id: "0-0",
+          content: "that's trash!!",
+          replies: []
+        },
+        {
+          id: "0-1",
+          content: "i love it!!!!!!!!",
+          replies: []
+        }
+      ]
+    }
+  ];
+
+  const [commentTree, setCommentTree] = useState(initialCommentTree);
+  const [replyStatus, setReplyStatus] = useState({ id: "0", status: false });
+  const [replyContent, setReplyContent] = useState("");
+
+  useEffect(() => {
+    // add a comment box in the dom
+    setCommentTree(commentTree);
+  }, [commentTree]);
+
+  const addReply = (parent_id: string): void => {
+    // toggle textarea based on id
+    return replyStatus.status
+      ? setReplyStatus({ id: parent_id, status: false })
+      : setReplyStatus({ id: parent_id, status: true });
+  };
+
+  const genId = (parent_id: string, reply: T_Comment): string => {
+    let new_id = "";
+    // check length of replies
+    let length: number = reply.replies.length;
+    if (length === 0) {
+      new_id = parent_id + "-" + 0;
+    } else {
+      new_id = parent_id + "-" + length;
+    }
+    return new_id;
+  };
+
+  const dfs = (obj: T_Comment, targetId: string): T_Comment | undefined => {
+    if (obj.id === targetId) {
+      return obj;
+    }
+    if (obj.replies) {
+      for (let item of obj.replies) {
+        let check = dfs(item, targetId);
+        if (check) {
+          return check;
+        }
+      }
+    }
+  };
+
+  const submitReply = (parent_id: string): void => {
+    let result: T_Comment | any = {};
+
+    for (let obj of commentTree) {
+      result = dfs(obj, parent_id);
+      if (result) {
+        break;
+      }
+    }
+
+    let new_id = genId(parent_id, result);
+
+    let newObj = {
+      id: new_id,
+      content: replyContent,
+      replies: []
+    };
+
+    result.replies.push(newObj);
+    setReplyStatus({ id: "0", status: false });
+    setReplyContent("");
+  };
+
+  const commenter = (comment: T_Comment): JSX.Element => (
+    <div key={comment.id} className="comment">
+      <div className="content">
+        <div>{comment.content}</div>
+        <button onClick={() => addReply(comment.id)}>Reply</button>
+      </div>
+      {replyStatus.status === true && replyStatus.id === comment.id ? (
+        <div className="reply-box">
+          <textarea
+            autoFocus
+            value={replyContent}
+            onChange={(e) => setReplyContent(e.target.value)}
+          />{" "}
+          <button
+            onClick={() => {
+              submitReply(comment.id);
+            }}
+          >
+            submit
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+      {comment.replies.map((reply) => {
+        return commenter(reply);
+      })}
+    </div>
+  );
+
+  return <>{commentTree.map((comment) => commenter(comment))}</>;
+}
